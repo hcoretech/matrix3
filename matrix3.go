d2cc837f1ba544fe9d5afe99987cbd794a465938
unknown <get2henry14@gmail.com>
2025-10-26 21:15:33 +0100

    new commit

--- matrix3.go
+++ matrix3.go
@@ -1,11 +1,11 @@
 package matrix3
 
 // Matrix3 represents a 3x3 matrix.
-type Matrix3 [3][3]float64
+type Mtx3 [3][3]float64
 
 // NewIdentity creates and returns an identity matrix.
-func NewIdentity() Matrix3 {
-	return Matrix3{
+func NewIdentity() Mtx3 {
+	return Mtx3{
 		{1, 0, 0},
 		{0, 1, 0},
 		{0, 0, 1},
